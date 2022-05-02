23bfba299fb8ee194383bd105a0ab54be0ecfdd5
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-05-02 14:45:22 +0800

    Extracted table into a reusable funciton

---
+++ src/common/userTable.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
+import Table from './table';
+
+const UserTable = ({ users, localEnums, sortColumn, onSort }) => {
+  const columns = [
+    { path: 'id', label: 'ID' },
+    {
+      key: 'name',
+      content: (user) => <Link to={'/users/' + user.id}>{user.name}</Link>,
+      label: 'Name',
+    },
+    { path: 'username', label: 'Username' },
+    { path: 'access', label: 'Access' },
+    { path: 'status', label: 'Status' },
+  ];
+
+  return (
+    <Table
+      columns={columns}
+      sortColumn={sortColumn}
+      onSort={onSort}
+      localEnums={localEnums}
+      data={users}
+    />
+  );
+};
+
+export default UserTable;
--- src/components/user.jsx
+++ src/components/user.jsx
@@ -2,80 +2,85 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Users from './../service/users.json';
+import Branches from './../service/branches.json';
 import Pagination from './../common/pagination';
 import { paginate } from '../utils/paginate';
+import ListGroup from './../common/listGroup';
+import UserTable from '../common/userTable';
+import _ from 'lodash';
 
 const User = () => {
   const [users, setUsers] = useState([]);
+  const [branches, setBranches] = useState([]);
   const [pageSize, setPageSize] = useState(25);
   const [currentPage, setCurrentPage] = useState(1);
+  const [sortColumn, setSortColumn] = useState({});
+  const [localEnums, setLocalEnums] = useState({
+    status: [
+      { id: 0, value: 'TEMPORARY' },
+      { id: 1, value: 'ACTIVE' },
+      { id: 2, value: 'DISABLED' },
+    ],
+    access: [
+      { id: 0, value: 'DEVELOPER' },
+      { id: 1, value: 'OIC' },
+      { id: 2, value: 'ASSISTANT OIC' },
+      { id: 3, value: 'MANAGEMENT' },
+      { id: 4, value: 'OPTOMETRIST' },
+      { id: 5, value: 'ENCODER' },
+      { id: 6, value: 'SALES' },
+    ],
+  });
 
   useEffect(() => {
     handlePageChange(0);
     setUsers(Users);
+    setBranches(Branches);
   }, []);
 
+  const handleSort = (sortColumn) => {
+    setSortColumn(sortColumn);
+  };
+
   const handlePageChange = (i) => {
-    // let item = {
-    //   index: i + 1,
-    //   items: [],
-    // };
-    // for (let k = i * pageSize; k < i * pageSize + pageSize; k++) {
-    //   if (!Users[k]) continue;
-    //   item.items.push({ ...Users[k] });
-    // }
-    // setCurrentPage(i + 1);
-    // setTableData(item.items);
     setCurrentPage(i + 1);
   };
 
-  let tableUsers = paginate(users, currentPage, pageSize);
+  const getPagedData = () => {
+    // const filtered
+
+    const sortedUsers = _.orderBy(users, [sortColumn.path], [sortColumn.order]);
+
+    return paginate(sortedUsers, currentPage, pageSize);
+  };
 
   return (
-    <div>
-      <h1 className='d-flex align-items-left'>USERS </h1>
-      <div className='d-flex align-items-left'>
-        <Link
-          to='/users/new'
-          className='btn btn-primary'
-          style={{ marginBottom: 20 }}
-        >
-          New User
-        </Link>
+    <div className='row'>
+      {/* <div className='col-2'><ListGroup items={ }/></div> */}
+      <div className='col'>
+        <h1 className='d-flex align-items-left'>USERS </h1>
+        <div className='d-flex align-items-left'>
+          <Link
+            to='/users/new'
+            className='btn btn-primary'
+            style={{ marginBottom: 20 }}
+          >
+            New User
+          </Link>
+        </div>
+        <UserTable
+          users={getPagedData()}
+          localEnums={localEnums}
+          onSort={handleSort}
+          sortColumn={sortColumn}
+        />
+        <Pagination
+          totalItems={users.length}
+          pageSize={pageSize}
+          onPageChange={handlePageChange}
+          currentPage={currentPage}
+        />
       </div>
-
-      <table className='table justify-content'>
-        <thead>
-          <tr>
-            <th scope='col'>Id</th>
-            <th scope='col'>Name</th>
-            <th scope='col'>Username</th>
-            <th scope='col'>Access</th>
-            <th scope='col'>Status</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tableUsers.map((item) => (
-            <tr key={item.id}>
-              <td>{item.id}</td>
-              <td>
-                <Link to={'/users/' + item.id}>{item.name}</Link>
-              </td>
-              <td>{item.username}</td>
-              <td>{item.access}</td>
-              <td>{item.status}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <Pagination
-        totalItems={users.length}
-        pageSize={pageSize}
-        onPageChange={(page) => {
-          handlePageChange(page);
-        }}
-        currentPage={currentPage}
-      />
     </div>
   );
 };
