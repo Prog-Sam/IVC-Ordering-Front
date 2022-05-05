55b8adbfdc45cdf1b869e9a9097230667323eb22
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-05-05 17:44:56 +0800

    Updated Select

--- src/components/userForm.jsx
+++ src/components/userForm.jsx
@@ -1,53 +1,96 @@
 import React, { useEffect, useState } from 'react';
 import Joi from 'joi-browser';
 import useForm from '../hooks/useForm';
-import Branches from '../services/branches.json';
+import { getBranches, getBranch } from './../services/branchService';
+import { getUser, saveUser } from './../services/userService';
 
 const UserForm = (props) => {
   const [branches, setBranches] = useState([]);
+  const [user, setUser] = useState({
+    id: '',
+    name: '',
+    branchKey: 0,
+    access: '',
+    username: '',
+    password: '',
+  });
+
+  const localEnums = {
+    status: [
+      { id: 0, name: 'TEMPORARY' },
+      { id: 1, name: 'ACTIVE' },
+      { id: 2, name: 'DISABLED' },
+    ],
+    access: [
+      { id: '0', name: 'DEVELOPER' },
+      { id: '1', name: 'OIC' },
+      { id: '2', name: 'ASSISTANT OIC' },
+      { id: '3', name: 'MANAGEMENT' },
+      { id: '4', name: 'OPTOMETRIST' },
+      { id: '5', name: 'ENCODER' },
+      { id: '6', name: 'SALES' },
+    ],
+  };
+
   const schema = {
     id: Joi.string().label('ID'),
     name: Joi.string().required().min(3).label('Name'),
-    branchKey: Joi.string().required().label('Branch Key'),
-    access: Joi.number().required().label('Access'),
+    branchKey: Joi.number().required().label('Branch Key'),
+    access: Joi.string().required().label('Access'),
     username: Joi.string().required().min(6).label('Username'),
     password: Joi.string().required().min(6).label('Password'),
+    status: Joi.number().required().label('Status'),
   };
 
   useEffect(() => {
-    const branchesInDb = Branches;
-    setBranches(branchesInDb);
+    async function populateBranches() {
+      let { data } = await getBranches();
+      setBranches(data);
+    }
+
+    populateBranches();
 
     const userId = props.match.params.id;
-    if (userId === 'new') return;
+    if (userId === 'New') return;
 
-    const user = { id: userId };
+    async function populateUser() {
+      let { data } = await getUser(userId);
+      setUser(data);
+    }
+    populateUser();
+
     if (!user) return props.history.replace('/not-found');
 
     return console.log('disconnect Server');
   }, []);
 
-  const doSubmit = () => {
-    // call server
-    console.log(values);
+  const doSubmit = async () => {
+    console.log(branches);
+    await saveUser(user);
+
+    this.props.history.push('/users');
   };
 
-  const [values, handleSubmit, renderButton, renderInput] = useForm(
-    schema,
-    doSubmit
-  );
+  const [
+    values,
+    handleSubmit,
+    renderButton,
+    renderInput,
+    renderLabel,
+    renderSelect,
+  ] = useForm(schema, doSubmit);
 
   return (
     <div>
-      <h1 className='d-flex align-items-left'>
-        REGISTER OR UPDATE USER{props.match.params.id}
-      </h1>
+      <h1 className='d-flex align-items-left'>REGISTER OR UPDATE USER</h1>
       <form onSubmit={handleSubmit}>
+        {renderLabel('ID', props.match.params.id)}
         {renderInput('name', 'Name')}
-        {renderInput('branchKey', 'Branch Key')}
-        {renderInput('access', 'Access')}
+        {renderSelect('branchKey', 'Branch', branches)}
+        {renderSelect('access', 'Access', localEnums.access)}
         {renderInput('username', 'Username')}
         {renderInput('password', 'Password', 'password')}
+        {renderSelect('status', 'Status', localEnums.status)}
         {renderButton('Submit')}
       </form>
     </div>
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,10 +1,34 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Input from './../common/input';
+import Select from '../common/select';
 import Joi from 'joi-browser';
 
 const useForm = (schema, submitCallback, itemInDb = {}) => {
   const [state, setState] = useState({});
   const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    // if (!itemInDb) return;
+    // let localState = {};
+    // let localSelState = {};
+    // let fields = Object.keys(itemInDb);
+    // for (let i = 0; i < fields.length; i++) {
+    //   if (
+    //     typeof itemInDb[fields[i]] === 'object' &&
+    //     itemInDb[fields[i]] !== null
+    //   ) {
+    //     localSelState[fields[i]] = itemInDb[fields[i]];
+    //     return;
+    //   }
+    //   localState[fields[i]] = itemInDb[fields[i]];
+    // }
+    // setState({ ...localState });
+    // setSelState({ ...localSelState });
+  }, []);
+
+  const getSelectOptions = (options) => {
+    return options.map((opt) => ({ label: opt.name, value: opt.id }));
+  };
 
   const validate = () => {
     const options = { abortEarly: false };
@@ -47,6 +71,26 @@ const useForm = (schema, submitCallback, itemInDb = {}) => {
     setErrors(localErrors);
   };
 
+  const handleSelectChange = ({ value, label }, { name }) => {
+    setState({
+      ...state,
+      [name]: value,
+    });
+
+    console.log(state);
+    // setSelState({
+    //   ...selState,
+    //   ['selVal' + name]: { label: label, value: value },
+    // });
+
+    const localErrors = { ...errors };
+    const errorMessage = validateProperty({ value, name });
+    if (errorMessage) localErrors[name] = errorMessage;
+    else delete localErrors[name];
+
+    setErrors(localErrors);
+  };
+
   const renderButton = (label) => {
     return (
       <button
@@ -79,7 +123,29 @@ const useForm = (schema, submitCallback, itemInDb = {}) => {
     );
   };
 
-  return [state, handleSubmit, renderButton, renderInput, renderLabel];
+  const renderSelect = (name, label, options) => {
+    return (
+      <Select
+        name={name}
+        label={label}
+        options={getSelectOptions(options)}
+        error={errors[name]}
+        value={getSelectOptions(options).filter(
+          (option) => option.value === state[name]
+        )}
+        onChange={handleSelectChange}
+      />
+    );
+  };
+
+  return [
+    state,
+    handleSubmit,
+    renderButton,
+    renderInput,
+    renderLabel,
+    renderSelect,
+  ];
 };
 
 export default useForm;
