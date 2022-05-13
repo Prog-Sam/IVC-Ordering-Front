f0c2f6f33f08bddb84135272ae31e84e0fbd0cee
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-05-13 15:31:56 +0800

    Extracted Menu Header

---
+++ src/common/menuHeader.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import SearchBox from './searchBox';
+
+const MenuHeader = ({ path, header, buttonLabel, searchQuery, onSearch }) => {
+  return (
+    <Fragment>
+      <h1 className='d-flex align-items-left'>{header}</h1>
+      <div className='d-flex align-items-left'>
+        <Link
+          to={`/${path}/New`}
+          className='btn btn-primary'
+          style={{ marginBottom: 20 }}
+        >
+          New {buttonLabel}
+        </Link>
+      </div>
+      <SearchBox value={searchQuery} onChange={onSearch} />
+    </Fragment>
+  );
+};
+
+export default MenuHeader;
