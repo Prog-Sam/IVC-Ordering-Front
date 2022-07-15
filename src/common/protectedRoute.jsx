dee61236ad8f98104ae202849a11d1f474c09128
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-07-15 11:25:34 +0800

    editted route protection for access level

--- src/common/protectedRoute.jsx
+++ src/common/protectedRoute.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { getCurrentUser } from '../services/authService';
+import { blacklisted, whitelisted } from '../utils/protector';
 
-const ProtectedRoute = ({ path, component: Component, render, ...rest }) => {
+const ProtectedRoute = ({
+  path,
+  component: Component,
+  render,
+  inclusion = [],
+  exclusion = [],
+  ...rest
+}) => {
+  const isAuthorized = (curUser, inc = [], exc = []) => {
+    console.log(inc);
+    console.log(curUser.access);
+    console.log(whitelisted(inc, curUser));
+    if (!curUser) return false;
+    if (inc) return whitelisted(inc, curUser) ? true : false;
+    if (exc) return blacklisted(inc, curUser) ? false : true;
+    return true;
+  };
   return (
     <Route
       {...rest}
       render={(props) => {
-        if (!getCurrentUser())
+        if (!isAuthorized(getCurrentUser(), inclusion, exclusion))
           return (
             <Redirect
               to={{ pathname: '/login', state: { from: props.location } }}
