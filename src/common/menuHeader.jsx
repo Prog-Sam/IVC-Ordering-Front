1a46b74d02ddb5b8bad383b42ea63fdee4e337f3
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-07-15 11:24:32 +0800

    Add Button, hidden for encapsulation

--- src/common/menuHeader.jsx
+++ src/common/menuHeader.jsx
@@ -1,19 +1,25 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import SearchBox from './searchBox';
+import { showIf } from '../utils/protector';
+import { getCurrentUser } from '../services/authService';
 
 const MenuHeader = ({ path, header, buttonLabel, searchQuery, onSearch }) => {
   return (
     <Fragment>
       <h1 className='d-flex align-items-left'>{header}</h1>
       <div className='d-flex align-items-left'>
-        <Link
-          to={`/${path}/New`}
-          className='btn btn-primary'
-          style={{ marginBottom: 20 }}
-        >
-          New {buttonLabel}
-        </Link>
+        {showIf(
+          <Link
+            to={`/${path}/New`}
+            className='btn btn-primary'
+            style={{ marginBottom: 20 }}
+          >
+            New {buttonLabel}
+          </Link>,
+          getCurrentUser(),
+          [0, 6, 7]
+        )}
       </div>
       <SearchBox value={searchQuery} onChange={onSearch} />
     </Fragment>
