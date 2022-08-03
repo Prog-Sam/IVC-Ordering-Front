9670314fb00348b957dec24bec4b31c43641cafb
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-08-03 14:57:25 +0800

    Added Ordering Navigation

--- src/OrderingApp.js
+++ src/OrderingApp.js
@@ -2,12 +2,16 @@ import React, { useState, useEffect } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Home from './components/CRUD/menu/home';
+import Order from './components/Ordering/orders/order';
+import OrderForm from './components/Ordering/orders/orderForm';
 
 import NotFound from './components/CRUD/menu/notFound';
 import Navigation from './components/Ordering/navigation/navigation';
 import LoginForm from './components/CRUD/login/loginForm';
 import UserContext from './context/userContext';
+import CartContext from './context/cartContext';
 import { getCurrentUser, logout } from './services/authService';
+import { getCart, storeCart } from './services/cartService';
 import ProtectedRoute from './common/protectedRoute';
 import access from './config/accessConfig.json';
 import 'react-toastify/dist/ReactToastify.css';
@@ -31,11 +35,16 @@ function OrderingApp() {
       storeCatalog(data);
     }
 
-    function populateOrdersCount() {}
+    function populateOrdersCount() {
+      const cart = getCart();
+      setOrdersCount(cart.length);
+    }
 
     if (token) {
       setCurrentUser(getCurrentUser());
       if (!getCatalog()) populateLocalCatalog();
+      if (!getCart()) storeCart([]);
+      populateOrdersCount();
     }
   }, []);
 
@@ -47,16 +56,27 @@ function OrderingApp() {
   return (
     <div className='OrderingApp'>
       <UserContext.Provider value={{ currentUser, setCurrentUser }}>
-        <Navigation currentUser={currentUser} onLogout={handleLogout} />
-        <ToastContainer />
-        <main className='container'>
-          <Switch>
-            <Route path='/login' render={(props) => <LoginForm {...props} />} />
-            <Route path='/not-found' component={NotFound}></Route>
-            <Route path='/' exact render={(props) => <Home {...props} />} />
-            <Redirect to='/not-found' />
-          </Switch>
-        </main>
+        <CartContext.Provider value={{ ordersCount, setOrdersCount }}>
+          <Navigation
+            currentUser={currentUser}
+            onLogout={handleLogout}
+            ordersCount={ordersCount}
+          />
+          <ToastContainer />
+          <main className='container'>
+            <Switch>
+              <ProtectedRoute path='/orders/:id' component={OrderForm} />
+              <ProtectedRoute path='/orders' component={Order} />
+              <Route
+                path='/login'
+                render={(props) => <LoginForm {...props} />}
+              />
+              <Route path='/not-found' component={NotFound}></Route>
+              <Route path='/' exact render={(props) => <Home {...props} />} />
+              <Redirect to='/not-found' />
+            </Switch>
+          </main>
+        </CartContext.Provider>
       </UserContext.Provider>
     </div>
   );
---
+++ src/components/Ordering/navigation/navigation.jsx
@@ -0,0 +1,52 @@
+import React, { Fragment } from 'react';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Navigation = ({ currentUser, onLogout, ordersCount }) => {
+  return (
+    <Navbar bg='dark' variant='dark' expand='lg'>
+      <Container>
+        <Navbar.Brand as={Link} to='/'>
+          IVC-ORDERING
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='me-auto'>
+            <Link to='/orders/New' className='nav-link form-inline'>
+              NEW ORDER
+            </Link>
+            <Link to='/orders' className='nav-link form-inline'>
+              CART{' '}
+              {ordersCount > 0 && (
+                <span class='badge badge-danger'>{ordersCount}</span>
+              )}
+            </Link>
+            <Link to='/catalog' className='nav-link'>
+              CATALOG
+            </Link>
+            <Link to='/status' className='nav-link'>
+              STATUS
+            </Link>
+            {currentUser && (
+              <Fragment>
+                <Link to='/#' className='nav-link'>
+                  {currentUser.name}
+                </Link>
+                <Link to='/#' className='nav-link ' onClick={onLogout}>
+                  LOGOUT
+                </Link>
+              </Fragment>
+            )}
+            {!currentUser && (
+              <Link to='/login' className='nav-link'>
+                LOGIN
+              </Link>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Navigation;
