98648568c4cfefa36050431d817df90776c52218
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-08-03 14:50:23 +0800

    Added Service for LocalCatalog

--- src/services/localCatalogService.js
+++ src/services/localCatalogService.js
@@ -1,6 +1,23 @@
 import http from './httpService';
+import store from '../utils/store';
+
+const catalogKey = 'localCatalog';
 
 export async function getLocalCatalog() {
   const localCatalog = await http.get(`/localCatalog`);
   return localCatalog;
+}
+
+export function storeCatalog(catalog) {
+  store.saveObject(catalogKey, catalog);
+  return 'Catalog Stored';
+}
+
+export function getCatalog() {
+  return store.getObject(catalogKey);
+}
+
+export function removeCatalog() {
+  store.removeObject(catalogKey);
+  return 'Catalog Removed';
 }
