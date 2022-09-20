e9e3ce7890b40b7498ef281bd97d4ca6319fecc6
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-09-20 18:35:14 +0800

    Added capability to distinguish Similar Item

--- src/services/orderItemService.js
+++ src/services/orderItemService.js
@@ -3,6 +3,7 @@ import store from '../utils/store';
 import _ from 'lodash';
 import { generateOrderId, generateOrderItemId } from '../utils/idGenerator';
 import { getCart, getOrder, updateOrder } from './cartService';
+import { isGradeDuplicate } from '../utils/gradeMethods';
 
 const cartKey = 'cart';
 
@@ -52,4 +53,18 @@ export function isDuplicate(orderId, itemId) {
   console.log(getItem(orderId, itemId));
   if (getItem(orderId, itemId)) return true;
   return false;
+}
+
+export function isSimilar(item1, item2) {
+  if (item1.rxNumber != item2.rxNumber) return false;
+  if (item1.orderTypeKey != item2.orderTypeKey) return false;
+  if (item1.supplyCategoryKey != item2.supplyCategoryKey) return false;
+  if (item1.brandKey != item2.brandKey) return false;
+  if (item1.itemKey != item2.itemKey) return false;
+  if (item1.itemDescription != item2.itemDescription) return false;
+  if (item1.cdKey != item2.cdKey) return false;
+  if (item1.additionalInstruction != item2.additionalInstruction) return false;
+  if (item1.pxName != item2.pxName) return false;
+  if (!isGradeDuplicate(item1.grade, item2.grade)) return false;
+  return true;
 }
