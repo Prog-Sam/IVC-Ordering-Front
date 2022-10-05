0942322d95b0700c41d221e675524b28a1724a93
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-10-05 18:39:29 +0800

    Submit for approval fully funcitonal

--- src/utils/orderMethods.js
+++ src/utils/orderMethods.js
@@ -1,10 +1,28 @@
 import { saveOrder } from '../services/orderService';
 import { toast } from 'react-toastify';
 import _ from 'lodash';
+import orderStatus from '../config/orderStatusConfig.json';
+import { removeOrder } from './../services/cartService';
 
-export function submitForApproval(orderId) {
-  const result = saveOrder(orderId);
+export async function submitForApproval(orderId) {
   toast('Submitting Order...');
+  const result = await saveOrder(orderId, orderStatus.forApproval);
+  const validatedResult = validateResult(result);
+
+  if (!validatedResult.isValid) {
+    toast.error(validatedResult.result.errorMessage);
+    console.error(validatedResult.result);
+    return false;
+  }
+
+  toast.success('Order has been submitted...');
+  removeOrder(orderId);
+  return result;
 }
 
-function validateResult(result) {}
+function validateResult(result) {
+  const resultPaths = Object.keys(result);
+  if (_.find(resultPaths, (p) => p == 'errorMessage'))
+    return { isValid: false, result };
+  return { isValid: true, result };
+}
