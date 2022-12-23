f44d551bae78895f783cbd98fb65f1c324cb25d4
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-12-23 11:14:32 +0800

    Faqs Added

--- src/components/Ordering/menu/faqs.jsx
+++ src/components/Ordering/menu/faqs.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import FilePicker from '../../../common/filePicker';
+import React, { useState, useEffect } from 'react';
+import { Accordion } from 'react-bootstrap-accordion';
+import { getPosts } from '../../../services/postService';
 
 const FAQs = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function populatePosts() {
+      const { data } = await getPosts('?type=2faqs');
+      setPosts(data);
+    }
+
+    populatePosts();
+  }, []);
   return (
     <div>
       <h1>FAQs</h1>
+      <h6>Frequently Asked Questions will be addressed here in this page.</h6>
+
+      {posts.map((p) => (
+        <Accordion title={p.title} children={p.message} />
+      ))}
     </div>
   );
 };
