ef34eae9beb532c5ce8cd9385f262561a0d17374
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-12-23 10:10:55 +0800

    Added Accordion css

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import OrderingApp from './OrderingApp';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
+import 'react-bootstrap-accordion/dist/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
