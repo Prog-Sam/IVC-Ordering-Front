1fc8bc879246810c7ca99afcb40f7188706cdc63
John Ardenn Sam M. Basco <bascosam217@gmail.com>
2022-12-23 11:15:20 +0800

    Home and Announcement Board Tied in

---
+++ src/common/announcementBoard.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import moment from 'moment';
+
+// const AnnouncementBoard = ({ items }) => {
+//   return (
+//     <div>
+//       <h3>Announcements</h3>
+//       {items.map((i) => (
+//         <div>
+//           <hr />
+//           <table className='table'>
+//             <thead>
+//               <row>
+//                 <th className='align-middle'>Posted by: {i.UserId.name}</th>
+//                 <th className='align-middle'>
+//                   Date: {moment(i.date).format('MM/DD/YYYY HH:mm:ss')}
+//                 </th>
+//               </row>
+//             </thead>
+//             <tbody>
+//               <row>
+//                 <td colSpan={2} className='align-middle'>
+//                   <h6 className='align-middle'>{i.title}</h6>
+//                 </td>
+//               </row>
+//               <row>
+//                 <td colSpan={2}>{i.message}</td>
+//               </row>
+//             </tbody>
+//           </table>
+//           <hr />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+const AnnouncementBoard = ({ items }) => {
+  return (
+    <div>
+      <br />
+      <h3>Announcements</h3>
+      {items.map((i) => (
+        <div>
+          <hr />
+          <div className='row'>
+            <div className='col'>Posted By: {i.UserId.name}</div>
+            <div className='col'>
+              Posted On: {moment(i.date).format('ddd MM/DD/YYYY HH:mm:ss')}
+            </div>
+          </div>
+          <br />
+          <div className='row'>
+            <div className='col col-span2'>
+              <h5>{i.title}</h5>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col col-span2'>{i.message}</div>
+          </div>
+        </div>
+      ))}
+      <hr />
+    </div>
+  );
+};
+
+export default AnnouncementBoard;
--- src/components/Ordering/menu/home.jsx
+++ src/components/Ordering/menu/home.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import FilePicker from '../../../common/filePicker';
+import React, { useState, useEffect } from 'react';
+import AnnouncementBoard from '../../../common/announcementBoard';
+import { getPosts } from '../../../services/postService';
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function populatePosts() {
+      const { data } = await getPosts('?type=2anns&includeUsers=true');
+      setPosts(data);
+    }
+
+    populatePosts();
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
       {/* <FilePicker /> */}
       Welcome to IVC-ORDERING...
+      <AnnouncementBoard items={posts} />
     </div>
   );
 };
