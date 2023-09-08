5ac1fee0a92609386378a47078782317fcc92332
Prog-Sam <54310091+Prog-Sam@users.noreply.github.com>
2023-09-08 17:26:01 +0800

    Dedicated to Ordering App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
 import OrderingApp from './OrderingApp';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -12,8 +11,8 @@ import 'react-bootstrap-accordion/dist/index.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
-      {/* <OrderingApp /> */}
+      {/* <App /> */}
+      <OrderingApp />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
